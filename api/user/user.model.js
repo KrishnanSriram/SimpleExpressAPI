61f306231a3ba53a0de646afae91ccd24c448f67
unknown <krishnan.sriram@hotmail.com>
2018-03-30 10:35:06 -0400

    Code changes to add regex validation for name and DOB

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -6,16 +6,20 @@ const User = new Schema({
   firstname: {
     type: 'string',
     required: true,
-    length: { min: 3, max: 32 }
+    length: { min: 3, max: 32 },
+    match: /^[a-zA-Z ]*$/
   },
   lastname: {
     type: 'string',
     required: true,
-    length: { min: 3, max: 32 }
+    length: { min: 3, max: 32 },
+    match: /^[a-zA-Z ]*$/
   },
   dob: {
     type: 'string',
-    required: true
+    required: true,
+    length: { min:8, max: 10},
+    match: /(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/]\d{4}/
   }
 });
 
